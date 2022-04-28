3762707623f058578f5defe91ba7e27f9601a5fb
unknown <sth1812@posteo.de>
2022-04-28 16:39:29 +0200

    script js

---
+++ script.js
@@ -0,0 +1,48 @@
+
+let currentPokemon;
+async function loadPokemon() {
+    let pokedexSearch = document.getElementById('pokedexSearch').value.toLowerCase();
+    let url = `https://pokeapi.co/api/v2/pokemon/${pokedexSearch}`;
+    let response = await fetch(url);
+    currentPokemon = await response.json();
+    console.log('loaded pokemon', currentPokemon);
+    showPokemonInfo();
+}
+
+
+function showPokemonInfo() {
+    if (currentPokemon['name'] == undefined) { document.getElementById('pokemonName').innerHTML = 'Which Pokemon do you want to search?' }
+    else {
+        document.getElementById('pokemonName').innerHTML = 'Name: ' + upperCase(currentPokemon['name']);
+        document.getElementById('pokemonNumber').innerHTML = 'Number: ' + (currentPokemon['id']);
+        pokemonType();
+    }
+}
+
+function upperCase(pokemonNameUpperCase) {
+    return pokemonNameUpperCase.charAt(0).toUpperCase() + pokemonNameUpperCase.slice(1);
+}
+
+function renderPokemon() {
+let loadedPokemon = document.getElementById('allPokemon');
+loadedPokemon = '';
+
+for (let i = 0; i < array.length; i++) {
+
+loadedPokemon.innerHTML += `<div></div>`
+
+
+}
+}
+/* this function sets the color of a Pokemon depending on its type */
+function pokemonType() {
+    let pokemonType = document.getElementById('pokemonType');
+    pokemonType.innerHTML = "";
+    for (let i = 0; i < currentPokemon['types'].length; i++) {
+        const type = currentPokemon['types'][i]['type']['name'];
+        pokemonType.innerHTML += `<h2 class="pokemon-${type}">${type}</h2>
+           `
+    }
+}
+
+
