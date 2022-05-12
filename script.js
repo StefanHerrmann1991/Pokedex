bcae313355d190f8133113f6cb66d2064a4b9dde
unknown <sth1812@posteo.de>
2022-05-12 08:59:58 +0200

    new Style

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let pokemons = [];
 
 async function searchPokemon() {
-    getPokemonByName(pokemonName);
+    let pokemon = getPokemonByName();
     showPokemonInfo(pokemon);
 }
 
@@ -38,9 +38,9 @@ function pokemonType(pokemon) {
  * @param {number} i - pokemon ID that is asked for
  * */
 
-async function getPokemonByName(pokemonName) {
+async function getPokemonByName() {
     
-    pokemonName  = document.getElementById('pokedexSearch').value;
+    let pokemonName  = document.getElementById('pokedexSearch').value;
     let url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
     let responsePokemon = await fetch(url);
     let pokemon = await responsePokemon.json();
@@ -127,3 +127,77 @@ function pokemonCardBigView() {
 
 } 
 
+/* function load() {
+    loadImages(25); //hier muss die Anzahl an Bildern rein
+    for (i = 0; i < images.length; i++) {
+        document.getElementById('img').innerHTML += `
+        <img id="${i}" onmouseenter="zoomIn(${i})" onmouseleave="zoomOut(${i})" onclick="showImg(${i})" class="img-box" src="${images[i]}"<img>
+     `}
+}
+function loadImages(numberOfPictures) { //die Funktion lädt Bilder der Form Zahl.jpg
+    for (let i = 1; i < numberOfPictures + 1; i++) {
+        images.push(`img/${i}.jpg`);
+    }
+
+}
+function zoomIn(i) {
+    document.getElementById(i).classList.add('zoom-in');
+}
+
+
+function zoomOut(i) {
+    document.getElementById(i).classList.remove('zoom-in');
+}
+
+function showImg(i) {
+    document.getElementById('big-img').innerHTML = `
+    <div class="dialogue-bg" id="black-screen"><div class="close-btn mgn-top mgn-right"><button class="button"  onclick="closeImg()"><span class="material-icons-outlined">close</span></button></div>
+    <div class="dialogue-bg-child"><button class="button mgn-left"  onclick="lastImg(${i})"><span class="material-icons-outlined">arrow_back</span></button>
+    <img class="img-box-big" id="black-screen-img">
+    <button class="button mgn-right"  onclick="nextImg(${i})"><span class="material-icons-outlined">arrow_forward</span></button> 
+    </div>
+    </div>
+     `
+    document.getElementById('black-screen-img').src = `img/${i + 1}.jpg`;
+
+}
+function nextImg(i) {
+    {
+        if (i < images.length - 1) {
+            i++;
+        } else {
+            i = 0;
+        }
+        document.getElementById('black-screen-img').innerHTML = '';
+        showImg(i);
+    }
+}
+
+function lastImg(i) {
+    if (i > 0) {
+        i--;
+    }
+    else {
+        i = images.length - 1;
+    }
+    document.getElementById('black-screen-img').innerHTML = '';
+    showImg(i);
+}
+
+function closeImg() {
+
+    document.getElementById('black-screen').classList.add('d-none');}
+
+
+
+/* i in currentImage umbennenen zum besseren Verständnis 
+ document.getElementById('black-screen').classList.remove('dialogue-bg');
+    document.getElementById('img-box-big').classList.remove('black-screen-img');*/
+
+/* function openImage() {
+    openImage(currentImage);
+    currentImage++;
+    if (currentImage == images.length) {
+        currentImage = 0;
+    }
+} */
